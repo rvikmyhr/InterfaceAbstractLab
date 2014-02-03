0e7a767953067fd45f95ad9541ae608c04e46e5c
Ron <rvikmyhr@my.wctc.edu>
2014-02-03 14:04:53 -0600

    Edits to lab1

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -8,21 +8,13 @@ import javax.swing.JOptionPane;
  * @author      your name goes here
  * @version     1.00
  */
-public class AdvancedJavaCourse {
-    String courseName;
-    private String courseNumber;
-    private double credits;
+public class AdvancedJavaCourse extends Course {
     private String prerequisites;
 
     public AdvancedJavaCourse(String courseName, String courseNumber) {
         this.setCourseName(courseName);
         this.setCourseNumber(courseNumber);
     }
-
-    public String getCapitalizedCourseName() {
-        return this.getCourseName().toUpperCase();
-    }
-
 
     public String getPrerequisites() {
         return prerequisites;
@@ -35,31 +27,5 @@ public class AdvancedJavaCourse {
             System.exit(0);
         }
         this.prerequisites = prerequisites;
-    }
-
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.setCredits(credits);
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
-    
+    }   
 }
---
+++ src/lab1/Course.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ron
+ */
+public abstract class Course {
+
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+
+    public String getCapitalizedCourseName() {
+        return this.getCourseName().toUpperCase();
+    }
+
+        public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        }
+    public final void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+
+    
+    public void setCredits(double credits) {
+        if (credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+}
